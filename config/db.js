bdeefa84ff9daa0cc14103f027e97734cf9cedd0
Ismael231196 <isdusagu23@gmail.com>
2024-09-16 18:18:40 -0500

    fix

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ dotenv.config({path: '.env'})
 
 const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASS ?? '',{
     host: process.env.BD_HOST, 
-    port: 3306,
+    port: 3307,
     dialect: 'mysql',
     define:{
         timestamp:true //es para que si un usuario se registra agrega dos cuadros mas uno va a decir cuando fue creado y el otro cuando fue actualizado
